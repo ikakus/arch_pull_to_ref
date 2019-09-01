545808f836486736cbc3e6e27fd50204546f1aa3
ikakus <idadiani88@gmail.com>
2019-09-01 10:53:58 +0300

    Click test

--- app/src/main/java/com/example/scrollsnadbox/MainActivity.kt
+++ app/src/main/java/com/example/scrollsnadbox/MainActivity.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Onclick {
+    override fun click(data: String) {
+        Toast.makeText(this, data, Toast.LENGTH_SHORT).show()
+    }
 
     private val myDataset: ArrayList<String> = arrayListOf()
     private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewAdapter: MyAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         (1..30).forEach { element -> myDataset.add(element.toString()) }
         viewManager = LinearLayoutManager(this)
         viewAdapter = MyAdapter(myDataset)
-
+        viewAdapter.setClickListener(this)
         recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
             // use this setting to improve performance if you know that changes
             // in content do not change the layout size of the RecyclerView
@@ -47,6 +51,11 @@ class MyAdapter(private val myDataset: List<String>) :
     // Each data item is just a string in this case that is shown in a TextView.
     class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
+    private var listener: Onclick? = null
+
+    fun setClickListener(listener: Onclick) {
+        this.listener = listener
+    }
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(
@@ -65,8 +74,17 @@ class MyAdapter(private val myDataset: List<String>) :
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         holder.textView.text = myDataset[position]
+        listener?.let {
+            holder.textView.setOnClickListener {
+                listener?.click(myDataset[position])
+            }
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = myDataset.size
+}
+
+interface Onclick {
+    fun click(data: String)
 }
