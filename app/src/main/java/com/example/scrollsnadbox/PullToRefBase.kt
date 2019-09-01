a269a19eceef6633bfb82b5219f2f00cbee0b7f6
ikakus <idadiani88@gmail.com>
2019-09-01 10:59:30 +0300

    Nested scrolling child

--- app/src/main/java/com/example/scrollsnadbox/PullToRefBase.kt
+++ app/src/main/java/com/example/scrollsnadbox/PullToRefBase.kt
@@ -282,10 +282,81 @@ abstract class PullToRefBase @JvmOverloads constructor(
         }
     }
 
+    // NestedScrollingChild
+
+    override fun setNestedScrollingEnabled(enabled: Boolean) {
+        mNestedScrollingChildHelper.isNestedScrollingEnabled = enabled
+    }
+
+    override fun isNestedScrollingEnabled(): Boolean {
+        return mNestedScrollingChildHelper.isNestedScrollingEnabled
+    }
+
+    override fun startNestedScroll(axes: Int): Boolean {
+        return mNestedScrollingChildHelper.startNestedScroll(axes)
+    }
+
+    override fun stopNestedScroll() {
+        mNestedScrollingChildHelper.stopNestedScroll()
+    }
+
+    override fun hasNestedScrollingParent(): Boolean {
+        return mNestedScrollingChildHelper.hasNestedScrollingParent()
+    }
+
+    override fun dispatchNestedScroll(
+        dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int,
+        dyUnconsumed: Int, offsetInWindow: IntArray?
+    ): Boolean {
+        return mNestedScrollingChildHelper.dispatchNestedScroll(
+            dxConsumed, dyConsumed,
+            dxUnconsumed, dyUnconsumed, offsetInWindow
+        )
+    }
+
+    override fun dispatchNestedPreScroll(
+        dx: Int,
+        dy: Int,
+        consumed: IntArray?,
+        offsetInWindow: IntArray?
+    ): Boolean {
+        return mNestedScrollingChildHelper.dispatchNestedPreScroll(
+            dx, dy, consumed, offsetInWindow
+        )
+    }
+
+    override fun onNestedPreFling(
+        target: View, velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        return dispatchNestedPreFling(velocityX, velocityY)
+    }
+
+    override fun onNestedFling(
+        target: View, velocityX: Float, velocityY: Float,
+        consumed: Boolean
+    ): Boolean {
+        return dispatchNestedFling(velocityX, velocityY, consumed)
+    }
+
+    override fun dispatchNestedFling(
+        velocityX: Float,
+        velocityY: Float,
+        consumed: Boolean
+    ): Boolean {
+        return mNestedScrollingChildHelper.dispatchNestedFling(velocityX, velocityY, consumed)
+    }
+
+    override fun dispatchNestedPreFling(velocityX: Float, velocityY: Float): Boolean {
+        return mNestedScrollingChildHelper.dispatchNestedPreFling(velocityX, velocityY)
+    }
+
+
     private fun canChildScrollUp(): Boolean {
         return if (mTarget is ListView) {
             ListViewCompat.canScrollList(mTarget as ListView, -1)
         } else mTarget?.canScrollVertically(-1) == true
     }
+
 
 }
